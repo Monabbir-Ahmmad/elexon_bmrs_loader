c29cba5bc2794c9cf9dfdf67c51954a94c421e92
Monabbir-Ahmmad <monabbir.ahmmad@gmail.com>
2024-04-17 01:10:20 +0600

    Add main.py file for fetching Elexon BM Report data and saving it to CSV

---
+++ main.py
@@ -0,0 +1,19 @@
+from scripts.elexon_bm_report import get_elexon_bm_report
+from scripts.event_manager import EventManager
+from scripts.csv_maker import CSVMaker
+import datetime
+
+def main():
+    event_manager = EventManager()
+    csv_maker = CSVMaker()
+
+    event_manager.subscribe("dataEmit", csv_maker)
+
+    start_date = datetime.datetime.now().strftime('%Y-%m-%dT00:00:00.000Z')
+    end_date = (datetime.datetime.now() + datetime.timedelta(days=7)).strftime('%Y-%m-%dT00:00:00.000Z')
+
+    get_elexon_bm_report(start_date, end_date, event_manager)
+
+
+if __name__ == '__main__':
+    main()
