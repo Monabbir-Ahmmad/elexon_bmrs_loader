ff99395c7eb8b3ca5a1187a89357ed99d96fe55c
Monabbir-Ahmmad <monabbir.ahmmad@gmail.com>
2024-04-17 16:27:54 +0600

    Refactor main.py to use loader_runner function from elexon_bm_report module

--- main.py
+++ main.py
@@ -1,19 +1,9 @@
-from scripts.loaders.elexon_bm_report import get_elexon_bm_report
-from scripts.event_manager import EventManager
-from scripts.csv_maker import CSVMaker
+from scripts.loaders.elexon_bm_report import loader_runner
+
 import datetime
 
 def main():
-    event_manager = EventManager()
-    csv_maker = CSVMaker(output_folder_path="curvefiles")
-
-    event_manager.subscribe("dataEmit", csv_maker)
-
-    start_date = (datetime.datetime.now() - datetime.timedelta(days=7)).strftime('%Y-%m-%dT00:00:00.000Z')
-    end_date = datetime.datetime.now().strftime('%Y-%m-%dT00:00:00.000Z')
-
-    get_elexon_bm_report(start_date, end_date, event_manager)
-
+    loader_runner()
 
 if __name__ == '__main__':
     main()
