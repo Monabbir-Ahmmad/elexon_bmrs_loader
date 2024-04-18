fcb6b161a8285d16b90ed7eb9d9cfbbb02bbc977
Monabbir-Ahmmad <monabbir.ahmmad@gmail.com>
2024-04-18 18:54:17 +0600

    Refactor CSVMaker to use dotenv for loading environment variables

--- scripts/utils/csv_maker.py
+++ scripts/utils/csv_maker.py
@@ -1,13 +1,29 @@
-from interfaces.subscriber import Subscriber
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
 import pandas as pd
+from interfaces.subscriber import Subscriber
+
+load_dotenv(join(dirname(__file__), ".env"))
 
 class CSVMaker(Subscriber):
-    def __init__(self, output_folder_path):
-        self.output_folder_path = output_folder_path
+    def __init__(self, output_file_name):
+        self.output_folder_path = os.getenv("BASE_OUTPUT_FOLDER")
+        self.output_file_name = output_file_name
+        
+        self.create_output_folder()
 
-    def update(self, data) -> None:
+    def create_output_folder(self):
+        if not os.path.exists(self.output_folder_path):
+            os.makedirs(self.output_folder_path)
+
+    def update(self, data):
         self.save_to_csv(data)
 
-    def save_to_csv(self, data) -> None:
-        df = pd.DataFrame(data)
-        df.to_csv(f'{self.output_folder_path}/output-{pd.Timestamp.now()}.csv', index=False)
+    def save_to_csv(self, data):
+        timestamp = pd.Timestamp.now().strftime("%Y-%m-%d %H-%M-%S")
+        file_name = f'{self.output_file_name}-{timestamp}.csv'
+
+        file_path = os.path.join(self.output_folder_path, file_name)
+
+        pd.DataFrame(data).to_csv(file_path, index=False)
